99f35d30f129a8bbaed744c8e4867adc1dc99811
Nguyen the phuong <nguyenphuong.it242@gmail.com>
2017-01-06 15:04:09 +0700

    camera feature

---
+++ camerafeature/index.ios.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableHighlight,
+} from 'react-native';
+
+// import camera
+import Camera from 'react-native-camera';
+
+export default class camerafeature extends Component {
+
+  clickeme(){
+    //alert("was touched");
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <TouchableHighlight onPress={this.clickeme.bind(this)}>
+
+            <View style={{height:50, width:50,backgroundColor:"pink"}}></View>
+          </TouchableHighlight>
+        </Camera>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+   preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('camerafeature', () => camerafeature);
